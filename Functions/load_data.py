60dab7238bf01a880d251f10e3a270ca3bc81366
bpoult <bpoult@uw.edu>
2021-05-19 18:22:21 -0700

    calibration should be working

--- Functions/load_data.py
+++ Functions/load_data.py
@@ -77,7 +77,8 @@ def load_data(save_dir,scan_name,ds_string, epix_roi, xrt_roi):
     RawData.changeValue(eventIDS = eventIDs, photon_energies=photon_energies,I0_fee = pulse_intensities,
                          low_diode_us=low_diode_us_events,high_diode_us=high_diode_us_events, epix_spectrum=epix_events,
                          xrt_spectrum=xrt_events,avg_epix_2d=np.asarray(epix_roiSum/i),xrt_intensity=np.sum(xrt_events,1),
-                         epix_intensity=np.sum(epix_events,1),scan_name=scan_name,epix_roi=epix_roi,xrt_roi=xrt_roi)
+                         epix_intensity=np.sum(epix_events,1),scan_name=scan_name,epix_roi=epix_roi,xrt_roi=xrt_roi,
+                        save_dir=save_dir,ds_string=ds_string)
 
     if not os.path.isdir(save_dir + scan_name):
         try:
--- raw_data_class.py
+++ raw_data_class.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import psana as ps
+import pickle
 
 
 class RawData:
@@ -40,22 +41,21 @@ class RawData:
 
     def energy_calib(self, calib):
         raw_data = self
-        xrt_pix = np.asarray([calib[2][x] for x in range(0,len(calib[2])) if calib[2][x] != 0])
+        xrt_pix = np.asarray([calib[2][x] for x in range(0, len(calib[2])) if calib[2][x] != 0])
         keV_xrt = np.asarray([calib[1][x] for x in range(0, len(calib[2])) if calib[2][x] != 0])
-        epix_pix = np.asarray([calib[3][x] for x in range(0,len(calib[3])) if calib[3][x] != 0])
+        epix_pix = np.asarray([calib[3][x] for x in range(0, len(calib[3])) if calib[3][x] != 0])
         keV_epix = np.asarray([calib[1][x] for x in range(0, len(calib[3])) if calib[3][x] != 0])
 
         m_xrt, b_xrt = np.polyfit(xrt_pix, keV_xrt, 1)
         m_epix, b_epix = np.polyfit(epix_pix, keV_epix, 1)
 
-        xrt_energy = m_xrt*np.arange(raw_data.xrt_roi[0],raw_data.xrt_roi[1])+b_xrt
+        xrt_energy = m_xrt * np.arange(raw_data.xrt_roi[0], raw_data.xrt_roi[1]) + b_xrt
         epix_energy = m_epix * np.arange(raw_data.epix_roi[2], raw_data.epix_roi[3]) + b_epix
 
-        calib[0].append(['xrt_energy','epix_energy'])
-        calib.append([xrt_energy,epix_energy])
+        calib[0].append(['xrt_energy', 'epix_energy'])
+        calib.append([xrt_energy, epix_energy])
         raw_data.changeValue(calibration=calib)
 
-
-        with open(save_dir + scan_name + '/' + "rawdata.pkl", "wb") as f:
-            pickle.dump(RawData, f)
+        with open(raw_data.save_dir + raw_data.scan_name + '/' + "rawdata.pkl", "wb") as f:
+            pickle.dump(raw_data, f)
         return raw_data
