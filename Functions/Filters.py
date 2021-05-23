10924237d2de8fce36847912e3eceb2975752a70
bpoult <bpoult@uw.edu>
2021-05-23 07:10:38 -0700

    filters and res_change + scaling are close

--- Functions/Filters.py
+++ Functions/Filters.py
@@ -8,7 +8,11 @@ import processed_data_class as PDC
 
 def filtering(raw_data, filters):
     pro_data = PDC
+    all_events = raw_data.eventIDs
     conditions = []
+    good_events = []
+    good_indicies = []
+    
     for i in range(0,len(filters)):
         if not filters[i][0]:
             conditions.append(True)
@@ -18,18 +22,23 @@ def filtering(raw_data, filters):
             conditions.append(lin_filter(raw_data,filters[i][2]))
         if filters[i][1] is 'rms':
             conditions.append(lin_filter(raw_data,filters[i][2]))
+        good_events.append(raw_data.eventIDs[conditions[i]])
+        
+        
+        
+        #good_indicies.append(all_events.searchsorted(good_events[i]))
 
-    good_events = [raw_data.eventIDs[conditions[filt]] for filt in range(0,len(filters))]
+            
+    
+    
 
 
 
-
-    return pro_data
+    return good_events,conditions
 
 
 def bounds_filter(raw_data,filt_param):
-    raw = raw_data.copy()
-    var = getattr(raw,filt_param[0])
+    var = getattr(raw_data,filt_param[0])
     lower_bound = filt_param[1]
     upper_bound = filt_param[2]
     num_stds = filt_param[3]
@@ -37,34 +46,35 @@ def bounds_filter(raw_data,filt_param):
     cond_low = var > np.median(var) - np.std(var)*num_stds
     cond_high = var < np.median(var) + np.std(var)*num_stds
     cond_abs_min = var > lower_bound
-    cond_abs_max = var < upper_bound
-
-    condition = cond_low and cond_high and cond_abs_min and cond_abs_max
+    if not upper_bound is 'None':
+        cond_abs_max = var < upper_bound
+    else:
+        cond_abs_max = True
+    condition = cond_low & cond_high & cond_abs_min & cond_abs_max
 
     return condition
 
 def lin_filter(raw_data,filt_param):
-    raw = raw_data.copy()
-    var_x = getattr(raw,filt_param[0][0])
-    var_y = getattr(raw,filt_param[0][1])
+    var_x = getattr(raw_data,filt_param[0])
+    var_y = getattr(raw_data,filt_param[1])
 
     lin_fit = np.polyfit(var_x, var_y, 1)
 
-    cond_lin_high = var_y < var_x * lin_fit[0] + lin_fit[1] + filt_param[1]
-    cond_lin_low = var_y > var_x * lin_fit[0] + lin_fit[1] - filt_param[1]
-    condition = cond_lin_high and cond_lin_low
+    cond_lin_high = var_y < var_x * lin_fit[0] + lin_fit[1] + filt_param[2]
+    cond_lin_low = var_y > var_x * lin_fit[0] + lin_fit[1] - filt_param[2]
+    
+    condition = cond_lin_high & cond_lin_low
 
     return condition
 
 def rms_filter(raw_data,filt_param):
 
-    raw = raw_data.copy()
     parameters = filt_param
 
     condition = 'tbd'
     return condition
 
-def plot_filters(raw,conditions,plot_on):
+def plot_filters(raw_data,conditions,plot_on):
 
 
     return
--- Functions/load_data.py
+++ Functions/load_data.py
@@ -74,7 +74,7 @@ def load_data(save_dir,scan_name,ds_string, epix_roi, xrt_roi):
     epix_events = np.asarray(epix_events)
     xrt_events = np.asarray(xrt_events)
     epix_motor = ps.Detector('CXI:DG2:MMS:10.RBV').__call__()
-    RawData.changeValue(eventIDS = eventIDs, photon_energies=photon_energies,pulse_energies_fee = pulse_energies,
+    RawData.changeValue(eventIDs = eventIDs, photon_energies=photon_energies,pulse_energies_fee = pulse_energies,
                          low_diode_us=low_diode_us_events,high_diode_us=high_diode_us_events, epix_spectrum=epix_events,
                          xrt_spectrum=xrt_events,avg_epix_2d=np.asarray(epix_roiSum/i),xrt_intensity=np.sum(xrt_events,1),
                          epix_intensity=np.sum(epix_events,1),scan_name=scan_name,epix_roi=epix_roi,xrt_roi=xrt_roi,
